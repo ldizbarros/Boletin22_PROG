9462a446787a83f04b8d8d17d45a256b1becdfeb
ldizbarros <ldizbarros@victoria13.danielcastelao.org>
2018-04-24 08:40:59 +0200

    Boletin 22 de Programacion

---
+++ src/boletin22/Boletin22.java
@@ -0,0 +1,12 @@
+package boletin22;
+
+public class Boletin22 {
+
+    public static void main(String[] args) {
+        Exercicio1 ventana = new Exercicio1();
+        ventana.setVisible(true);
+        Exercicio2 ventana2 = new Exercicio2();
+        ventana2.setVisible(true);
+    }
+    
+}
